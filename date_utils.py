5eae6831487b0592285d570cfb9fa7a4eedb3446
changbodeng <changbodeng@tencent.com>
2022-07-27 20:30:08 +0800

    feat(date_utils): add past_n_months

--- date_utils.py
+++ date_utils.py
@@ -1,8 +1,16 @@
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
 def past_n_days(venus_ds, day_cnt=0, output_format="%Y%m%d"):
     ds = datetime.strptime(str(venus_ds), "%Y%m%d") + timedelta(days=day_cnt)
+    return ds.strftime(output_format)
+
+
+def past_n_months(venus_month_ds, month_cnt=0, output_format="%Y%m"):
+    month_ds = str(venus_month_ds)[:6]
+
+    ds = datetime.strptime(month_ds, "%Y%m") + relativedelta(months=month_cnt)
     return ds.strftime(output_format)
 
 
